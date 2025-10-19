bfa9e767ab723e60fb9dfffa449b23723448a7b5
NoemieCERRINA <noemie.cerrina@gmail.com>
2025-10-19 17:57:45 +0200

    Ajout chrono

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <chrono>
 #include <string.h>
 #include "Constraint.h"
 #include "CSP.h"
 #include "utils.h"
+
+using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
@@ -18,13 +22,19 @@ int main(int argc, char *argv[])
     // csp = CSP(filename);
     int n = 16;
     csp = Queens_generator(n);
-    //vector<int> domain_last_elts = csp.AC4();
+    // vector<int> domain_last_elts = csp.AC4();
     csp.print();
     // for (int elt : domain_last_elts)
     //    cout << elt << ", ";
 
+    auto start_timer = high_resolution_clock::now();
+
     auto result = csp.MAC(&CSP::AC3);
-    //auto result = csp.fullFC();
+    // auto result = csp.fullFC();
+
+    auto stop_timer = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop_timer - start_timer);
+    cout << "Temps de resolution: " << duration.count() / 1000.0 << " secondes" << endl;
 
     cout << "Resultat: " << result.first << "\n";
     if (result.first)
